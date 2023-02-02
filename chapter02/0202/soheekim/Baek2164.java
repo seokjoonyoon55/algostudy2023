e0c3a0e294266205b40816972ff7e10afdbd73db
Sohee-Kim-ln <livre6491@gmail.com>
2023-02-02 11:16:14 +0900

    update codes and comments 230202 11:16am

--- chapter02/0202/soheekim/Baek2164.java
+++ chapter02/0202/soheekim/Baek2164.java
@@ -7,34 +7,33 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Baek2164 {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
-		Queue<Integer> quu = new LinkedList<>();
-		StringBuffer sb = new StringBuffer();
 		
-		String[] nk = bfr.readLine().split(" ");
-		int N=Integer.parseInt(nk[0]);
-		int K=Integer.parseInt(nk[1]);
+		Queue<Integer> quu = new LinkedList<>();//카드 저장할 큐
+		boolean doRemove = true;//true면 제거 실행, false면 밑으로 옮기기 실행
 		
-		int count=0;
+		int N = Integer.parseInt(bfr.readLine());//카드 갯수 저장
 		
-		for(int i=1;i<=N;i++) {
+		//1부터 N까지 순서대로 큐에 카드숫자 저장
+		for(int i=1;i<=N;i++)
+		{
 			quu.add(i);
 		}
 		
+		//quu가 2이상일 동안 실행
 		while(quu.size()>1) {
-			count++;
-			if(count!=K) {
-				quu.add(quu.poll());
-			}
+			//deRemove 상태에 따라 true면 제거 실행, false면 밑으로 옮기기 실행
+			if(doRemove) {
+				quu.poll();
+				doRemove=false;
+				}
 			else {
-				sb.append(quu.poll()+", ");
-				count=0;
+				quu.add(quu.poll());
+				doRemove=true;
 			}
 		}
-		
-		//마지막 남은 수 스트링버퍼에 붙이고 출력
-		sb.append(quu.poll());
-		System.out.println("<"+sb+">");
+		//1개만 남았을 때 제일 위에꺼 출력
+		System.out.println(quu.peek());
 	}
 }
