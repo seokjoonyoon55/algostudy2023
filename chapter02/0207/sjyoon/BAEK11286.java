319af2d25bb71b3f339e02cfd0fcd23edd68a7a8
unknown <sjyoon4755@gmail.com>
2023-02-07 18:03:39 +0900

    update

---
+++ chapter02/0207/sjyoon/BAEK11286.java
@@ -0,0 +1,21 @@
+package algo;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class BAEK11286 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		// 낮은 숫자가 우선 순위인 큐
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		for (int i=0;i<N;i++) {
+			
+		}
+		
+	}
+}
