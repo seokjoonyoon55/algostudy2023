0a1bd46a8e1ab50d8aae414fd6661b4db4f6979d
sjyoon <sjyoon4755@gmail.com>
2023-02-14 17:55:54 +0900

    update

---
+++ chapter03/0216/sjyoon/BAEK15500.java
@@ -0,0 +1,40 @@
+package algostudy;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BAEK15500 {
+	public static void main(String[] args) {
+		
+		Stack<Integer> st1 = new Stack<>();
+		Stack<Integer> st2 = new Stack<>();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for (int i=0;i<N;i++) {
+			st1.add(sc.nextInt());	
+		}
+		
+		for (int i=N;i>=1;i--) {
+			
+			if (st2.indexOf(N) == -1) {
+				st1.indexOf(N);
+				for ()
+				
+				
+			} else {
+				st2.indexOf(N);
+			}
+			
+			
+			
+		}
+		
+		
+	}
+	// 1번째, 2번째 장대에 세팅되어 있는 원판 무더기에서 제일 큰 숫자인 max 원판을 3번째 장대에 옮겨놓는 
+//	static int hanoi(int max) {
+	//}
+}
