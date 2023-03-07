191d594b1ea2ca8b8a5d45e7edbe5447b9e7a767
YOUNPRIZE <ysang10@gmail.com>
2023-03-08 08:02:30 +0900

    solved

--- chapter05_아침스터디/0307_구현/younsang/BAEK14719.java
+++ chapter05_아침스터디/0307_구현/younsang/BAEK14719.java
@@ -20,18 +20,22 @@ public class BAEK14719 {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		int cnt = 0;
+		// 양 끝은 어차피 물 안고임
 		for (int i = 1; i < W - 1; i++) {
 			int left = 0;
 			int right = 0;
 			
+			// 첫 칸 부터 해당 칸보다 바로 왼쪽 칸 사이에서 가장 큰 값
 			for (int j = 0; j < i; j++) {
 				left = Math.max(arr[j], left);
 			}
 			
+			// 해당 칸보다 바로 오른쪽 칸부터 끝 칸 사이에서 가장 큰 값
 			for (int j = i + 1; j < W; j++) {
 				right = Math.max(arr[j], right);
 			}
 			
+			// 두 left, right 변수보다 크기가 작으면 물이 코이므로 빼준 값 만큼 cnt에 더해준다.
 			if (arr[i] < left && arr[i] < right) cnt += Math.min(left, right) - arr[i];
 		}
 		System.out.println(cnt);
