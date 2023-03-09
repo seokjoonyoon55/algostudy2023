4ac2715b3b87ca88b52e5757c5545adbd8ad9207
Jaemin-Park9708 <catbirdseat9708@gmail.com>
2023-03-09 11:23:48 +0900

    230309 23:23 pjm commit at ssafy

--- chapter04_알고리즘 심화/0309_백트래킹/jaeminpark/baekjoon14889.java
+++ chapter04_알고리즘 심화/0309_백트래킹/jaeminpark/baekjoon14889.java
@@ -3,8 +3,6 @@ package baekjoon_02_Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class baekjoon14889 {
--- chapter04_알고리즘 심화/0309_백트래킹/jaeminpark/baekjoon15649.java
+++ chapter04_알고리즘 심화/0309_백트래킹/jaeminpark/baekjoon15649.java
@@ -9,16 +9,20 @@ import java.util.StringTokenizer;
 
 public class baekjoon15649 {
 
-	static int[] num;
-	static boolean[] isUsed;
-	static int[] ans;
+	// 1부터 N까지 자연수
 	static int N;
+	// M개의 수를 뽑아서 배열 만들어야 됨
 	static int M;
+	// 1부터 N까지 자연수로 만든 배열
+	static int[] num;
+	// 그 배열 중 사용한 자연수를 표시할 배열
+	static boolean[] isUsed;
+	// 조건을 만족하는 수열
+	static int[] ans;
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
@@ -28,6 +32,7 @@ public class baekjoon15649 {
 		for (int i = 0; i < N; i++) {
 			num[i] = i + 1;
 		}
+		// 일반적인 백트레킹
 		bt(0);
 	}
 
@@ -48,15 +53,6 @@ public class baekjoon15649 {
 				bt(depth + 1);
 				isUsed[i] = false;
 			}
-//			System.out.println(Arrays.toString(ans));
-//			System.out.println("------------------------");
 		}
-//		for (int i = 0; i < ans.length; i++) {
-//			bw.write(Integer.toString(ans[i]));
-//			bw.write(" ");
-//			bw.flush();
-//		}
-//		System.out.println();
-//		System.out.println("=================");
 	}
 }
