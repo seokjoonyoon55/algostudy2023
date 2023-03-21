3e1e74d461641e25fc129b6edf4ff8286ad8f418
Sohee-Kim-ln <livre6491@gmail.com>
2023-03-21 23:58:00 +0900

    solved 230321 11:57pm soheekim

---
+++ chapter05_아침스터디/0322_구현/soheekim/Baek14500.java
@@ -0,0 +1,171 @@
+package study0322;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//36420KB 680ms
+public class Baek14500 {
+
+	// 우 하 좌 상
+	static int[] dr = { 0, 1, 0, -1 };
+	static int[] dc = { 1, 0, -1, 0 };
+
+	static int N;// 행 r 수
+	static int M;// 열 c 수
+	static int[][] board;// 숫자 저장 배열
+	static boolean[][] visited;// 방문 여부 저장 배열
+	static int maxSum = Integer.MIN_VALUE;// 합 최대
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		String[] NM = bfr.readLine().split(" ");
+		N = Integer.parseInt(NM[0]);// 행 수
+		M = Integer.parseInt(NM[1]);// 열 수
+
+		// 숫자 배열, 방문여부 배열 할당
+		board = new int[N][M];
+		visited = new boolean[N][M];
+
+		// 숫자 입력받기 (String으로 받음)
+		String[][] tempS = new String[N][M];
+		for (int i = 0; i < N; i++) {
+			tempS[i] = bfr.readLine().split(" ");
+		}
+
+		// String->int로 저장
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				board[i][j] = Integer.parseInt(tempS[i][j]);
+			}
+		}
+
+		// ㅁ형 계산
+		 calSquare();
+
+		// 나머지 모양 백트래킹으로 계산
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				backR(i, j, 0, 0);//하향 우향만 하는 백트래킹
+				backL(i, j, 0, 0);//하향 좌향만 하는 백트래킹
+				calT(i,j);//T자형 계산
+			}
+		}
+
+		System.out.println(maxSum);
+
+	}
+
+	// 하 우
+	public static void backR(int r, int c, int sum, int length) {
+
+		// 4개 다 세면 최대합 갱신
+		if (length == 3) {
+			if (maxSum < sum)
+				maxSum = sum;
+			return;
+		}
+
+		// 시작점이면 sum에 시작값 저장
+		if (length == 0) {
+			sum = board[r][c];
+		}
+
+		// 델타탐색 우0, 하1 실행
+		for (int i = 0; i < 2; i++) {
+			// 경계조건
+			if (r + dr[i] >= N || r + dr[i] < 0 || c + dc[i] >= M || c + dc[i] < 0)
+				continue;
+			// 다음칸이 방문 안됐으면
+			if (!visited[r + dr[i]][c + dc[i]]) {
+				// 방문표시 후 합에 더함
+				visited[r + dr[i]][c + dc[i]] = true;
+				sum += board[r + dr[i]][c + dc[i]];
+
+				// 다음칸에 대해 백트래킹
+				backR(r + dr[i], c + dc[i], sum, length + 1);
+
+				// 되돌림
+				sum -= board[r + dr[i]][c + dc[i]];
+				visited[r + dr[i]][c + dc[i]] = false;
+			}
+		}
+	}
+
+	// 하 좌
+	public static void backL(int r, int c, int sum, int length) {
+		// 4개 다 세면 최대합 갱신
+		if (length == 3) {
+			if (maxSum < sum)
+				maxSum = sum;
+			return;
+		}
+
+		// 시작점이면 sum에 시작값 저장
+		if (length == 0) {
+			sum = board[r][c];
+		}
+
+		// 델타탐색 하1, 좌2 실행
+		for (int i = 1; i < 3; i++) {
+			// 경계조건
+			if (r + dr[i] >= N || r + dr[i] < 0 || c + dc[i] >= M || c + dc[i] < 0)
+				continue;
+			// 다음칸이 방문 안됐으면
+			if (!visited[r + dr[i]][c + dc[i]]) {
+				// 방문표시 후 합에 더함
+				visited[r + dr[i]][c + dc[i]] = true;
+				sum += board[r + dr[i]][c + dc[i]];
+
+				// 다음칸에 대해 백트래킹
+				backL(r + dr[i], c + dc[i], sum, length + 1);
+
+				// 되돌림
+				sum -= board[r + dr[i]][c + dc[i]];
+				visited[r + dr[i]][c + dc[i]] = false;
+			}
+		}
+	}
+
+	// 정사각형
+	public static void calSquare() {
+		int sum = 0;
+		
+		for (int i = 0; i < N - 1; i++) {
+			for (int j = 0; j < M - 1; j++) {
+				//4칸 합 계산 후 최대값 갱신
+				sum = board[i][j] + board[i + 1][j] + board[i][j + 1] + board[i + 1][j + 1];
+				if (maxSum < sum)
+					maxSum = sum;
+			}
+		}
+	}
+
+	// T자형
+	public static void calT(int r, int c) {
+		int sum = 0;// 합
+		int temp = -1;// 계산용 임시변수
+
+		// 4방향에 대해 i번째 방향이 없는 경우 계산
+		for (int i = 0; i < 4; i++) {
+			// 가운데값으로 초기화
+			sum = board[r][c];
+
+			// i번쨰 방향을 제외한 나머지 방향을 더해줌
+			for (int j = 1; j <= 3; j++) {
+				temp = (i + j) % 4;// 방향 재계산
+
+				// 경계조건
+				if (r + dr[temp] >= N || r + dr[temp] < 0 || c + dc[temp] >= M || c + dc[temp] < 0)
+					break;
+
+				// i번째 방향이 아닌 방향을 합에 저장
+				sum += board[r + dr[temp]][c + dc[temp]];
+
+			}
+			// 최대합 갱신
+			if (maxSum < sum)
+				maxSum = sum;
+		}
+	}
+}
--- chapter05_아침스터디/0322_구현/soheekim/Baek14890.java
+++ chapter05_아침스터디/0322_구현/soheekim/Baek14890.java
@@ -5,10 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Baek14890 {
-	static int[][] land;
-	static int N;
-	static int L;
-	static int cnt = 0;
+	static int[][] land;//땅 높이 저장 배열
+	static int N; //땅 변 길이
+	static int L; //경사로 길이
+	static int cnt = 0;//지날 수 있는 길 카운트
+	
 	// 우 하 좌 상
 	static int[] dr = { 0, 1, 0, -1 };
 	static int[] dc = { 1, 0, -1, 0 };
@@ -16,83 +17,100 @@ public class Baek14890 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
 		String[] NL = bfr.readLine().split(" ");
-		N = Integer.parseInt(NL[0]);
-		L = Integer.parseInt(NL[1]);
+		N = Integer.parseInt(NL[0]);//땅 변 길이
+		L = Integer.parseInt(NL[1]);//경사로 길이
 
-		land = new int[N][N];
+		land = new int[N][N];//땅 배열 할당
+		
+		//땅 String으로 입력받기
 		String[][] tempS = new String[N][N];
 		for (int i = 0; i < N; i++) {
 			tempS[i] = bfr.readLine().split(" ");
 		}
 
-		// 땅 입력받기
+		// 땅 int로 변환해서 저장
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				land[i][j] = Integer.parseInt(tempS[i][j]);
 			}
 		}
 
+		//시작점이 맨 왼쪽줄일 때
 		for (int r = 0; r < N; r++) {
 			calSmooth(r, 0, 0);// headDown 0 우향
 		}
 
+		//시작점이 맨 윗줄일 때
 		for (int c = 0; c < N; c++) {
 			calSmooth(0, c, 1);// headDown 1 하향
 		}
+		
+		//출력
 		System.out.println(cnt);
 
 	}
 
-	// headDown 0이면 우향 1이면 하향
+	//r,c 부터 시작해서 1칸씩 옮겨가며 1차이일 경우 경사로 놓는 메서드
+	// headDown 0이면 우향, 1이면 하향
 	public static void calSmooth(int r, int c, int headDown) {
-		//System.out.println("r c " + r + " " + c);
-		boolean smooth = true;
-		boolean[] ramp = new boolean[N];
+		boolean smooth = true;//지나갈 수 있는지 여부
+		boolean[] ramp = new boolean[N];//경사로 놓은 곳 표시 배열
 
-		int curR = r;
-		int curC = c;
+		int curR = r;//현재 열
+		int curC = c;//현재 행
 
-		int diff = 0;
-		boolean success = true;
+		int diff = 0;//땅높이 차이
+		boolean success = true;//경사로 놓기 성공 여부
 
-		// 위>아래 / 왼>오로 가면서 1낮아지는거 찾아서 경사로 놓기
+		// 위>아래 / 왼>오로 가면서 1낮은 or 1높은 곳 찾아서 경사로 놓기
 		for (int line = 0; line < N - 1; line++) {
-			success = true;
+			success = true;//초기화
 
+			//땅 높이차 계산
 			diff = land[curR][curC] - land[curR + dr[headDown]][curC + dc[headDown]];
+			
 			// -_이면
 			if (diff == 1) {
+				//다음 위치부터 진행하던 방향으로 경사로 놓기. 실패하면 메서드 종료
 				success = installRamp(curR + dr[headDown], curC + dc[headDown], headDown, ramp);
 				if (!success)
 					return;
 			}
 			// _-이면
 			else if (diff == -1) {
+				//현재 위치부터 진행하던 방향과 반대로 경사로 놓기. 실패하면 메서드 종료
 				success = installRamp(curR, curC, headDown + 2, ramp);
 				if (!success)
 					return;
 			}
-			//높이차 크면
+			//높이차가 1보다 크면 크면
 			else if (diff < -1 || diff > 1)
 				return;
+			//높이차 0은 아무 것도 안하고 넘어감
 
+			//다음칸으로 포인터 옮김
 			curR += dr[headDown];
 			curC += dc[headDown];
 		}
-		//System.out.println("r c " + r + " " + c + " success");
+
+		//중간 종료 안됐으므로 지나갈 수 있는 길 카운트 ++
 		cnt++;
 
 	}
 
+	//경사로 놓는 메서드
 	public static boolean installRamp(int r, int c, int direction, boolean[] ramp) {
+		//현재 위치와 현재 높이
 		int curR = r;
 		int curC = c;
 		int height = land[r][c];
 
+		//진행 방향에 따라 경사로 설치 여부  가리키는 인덱스 계산
 		int curB = direction % 2 == 0 ? c : r;
 		int idx = curB;
 		int add = direction < 2 ? 1 : -1;
 
+		//경사로 길이만큼 평지 판단
 		for (int i = 0; i < L; i++) {
 			// 경계조건
 			if (curR < 0 || curR >= N || curC < 0 || curC >= N)
@@ -107,12 +125,16 @@ public class Baek14890 {
 				return false;
 			// 높이 유지되면
 			else {
+				//포인터를 다음 칸으로 진행
 				curR += dr[direction];
 				curC += dc[direction];
+				
+				//현재 진행방향에 따른 경사로 설치여부 배열의 인덱스 계산
 				curB = direction % 2 == 0 ? curC : curR;
 			}
 		}
-
+		
+		//경사로 놓는데 최종 결과로 성공했으므로, 경사로 설치를 true로 표시하고, 
 		for (int i = 0; i < L; i++) {
 			ramp[idx] = true;
 			idx += add;
